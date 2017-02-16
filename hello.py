a66a3d598b8ad4d5a4552321ba095c455e015733
Nathan Danielsen <nathan.danielsen@gmail.com>
2017-02-15 21:06:12 -0500

    initial commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+import requests
+from flask import Flask
+from flask import render_template
+
+from jinja2 import Template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    url = 'https://www.opm.gov/json/operatingstatus.json'
+    r = requests.get(url)
+
+    data = r.json()
+    if data.get('StatusType', None) == 'Open':
+        data['background'] = 'red'
+    else:
+        data['background'] = 'green'
+
+
+    return render_template('opm.html', **data)
+if __name__ == "__main__":
+    app.run(debug=True)
